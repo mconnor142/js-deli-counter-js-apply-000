e8b73344812f26b603555f3ae0bab09b0f188972
Maxwell Connor <mconnor142@gmail.com>
2019-08-27 12:06:42 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,37 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeli, name){
+  katzDeli.push(`${name}`);
+  return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
+
+var i = 0;
+function takeANumberTwo(){
+  i++;
+  console.log(`Welcome, you are customer ${i}.`);
+}
+takeANumberTwo();
+takeANumberTwo();
+takeANumberTwo();
+takeANumberTwo();
+
+function nowServing(katzDeli){
+  if(katzDeli.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    for(var i = 0; i < katzDeli.length; i++){
+    return (`Currently serving ${katzDeli.shift()}.`);
+    }
+  }
+}
+function currentLine(katzDeli){
+  if(katzDeli.length === 0) {
+    return "The line is currently empty."
+  } else {
+    var array = [];
+    for (var i = 0; i < katzDeli.length; i++){
+      array.push(` ${i+1}. ${katzDeli[i]}`)
+    }
+    return `The line is currently:` + array;
+  }
+}
